723dad68c53f1fd1e31dfc71b875f72c574c6022
Coder0712 <84794107+Coder0712@users.noreply.github.com>
2026-02-22 23:10:21 +0100

    add project aggregate

---
+++ ProjectManagement.Domain/Projects/BoardId.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using ProjectManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Domain.Projects
+{
+    public sealed class BoardId : ValueObject
+    {
+        private BoardId(Guid value)
+        {
+            Value = value;
+        }
+
+        public Guid Value { get; init; }
+
+        public static BoardId Create(Guid id)
+        {
+            if(id == Guid.Empty)
+            {
+                throw new ArgumentException("The id can not be empty.", nameof(id));
+            }
+
+            return new BoardId(id);
+        }
+
+        public override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+    }
+}
