2a923acf8497796262d89caa774f41cd6462db17
Coder0712 <84794107+Coder0712@users.noreply.github.com>
2026-02-22 23:09:55 +0100

    add basic classes for ddd

---
+++ ProjectManagement.Domain/Common/AggregateRoot.cs
@@ -0,0 +1,32 @@
+﻿namespace ProjectManagement.Domain.Common
+{
+    /// <summary>
+    /// The aggregate root.
+    /// </summary>
+    public abstract class AggregateRoot : Entity
+    {
+        /// <summary>
+        /// For EFCore.
+        /// </summary>
+        protected AggregateRoot()
+        {
+        }
+
+        /// <summary>
+        /// Creates an new aggregate root.
+        /// </summary>
+        /// <param name="id">The id of the aggregate root.</param>
+        protected AggregateRoot(Guid id)
+            : base(id)
+        {
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public DateTime CreatedAt { get; init; }
+
+        public DateTime LastModified { get; protected set; }
+
+        public DateTime Modify()
+            => LastModified = DateTime.UtcNow;
+    }
+}
